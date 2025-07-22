8a3967b120764b13b5856a4ca86a48eda7964cb5
Prakriti Acharya <prakriti@acharya.com>
2025-07-22 05:55:24 -0400

    Added code that holds movie elelment data

--- app/src/main/java/ca/georgiancollege/movie_app/Movie.java
+++ app/src/main/java/ca/georgiancollege/movie_app/Movie.java
@@ -1,4 +1,25 @@
 package ca.georgiancollege.movie_app;
 
 public class Movie {
+    private String Title;
+    private String Year;
+    private String imdbID;
+    private String Type;
+    private String Poster;
+
+    // Constructor
+    public Movie(String title, String year, String imdbID, String type, String poster) {
+        this.Title = title;
+        this.Year = year;
+        this.imdbID = imdbID;
+        this.Type = type;
+        this.Poster = poster;
+    }
+
+    // Getters
+    public String getTitle() { return Title; }
+    public String getYear() { return Year; }
+    public String getImdbID() { return imdbID; }
+    public String getType() { return Type; }
+    public String getPoster() { return Poster; }
 }
